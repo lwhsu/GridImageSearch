581b66d9f45c21b0bf36647add09af1bfeac88dd
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-11-24 05:20:52 +0800

    Add OnItemSelectedListener to Spinners

--- src/org/lwhsu/android/gridimagesearch/activities/SettingActivity.java
+++ src/org/lwhsu/android/gridimagesearch/activities/SettingActivity.java
@@ -6,37 +6,96 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SettingActivity extends Activity {
+        private Spinner spSize;
+        private Spinner spColor;
+        private Spinner spType;
+
+        private String argSize;
+        private String argColor;
+        private String argType;
+
+        private final String[] optionsSize = {"small", "medium", "large", "xlarge"};
+        private final String[] optionsColor = {"black", "blue", "brown", "gray", "green", "orange", "pink", "purple", "red", "teal", "white", "yellow"};
+        private final String[] optionsType = {"face", "photo", "clipart", "lineart"};
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        setupView();
+        setupViews();
     }
 
-    private void setupView() {
-        final Spinner spSize = (Spinner) findViewById(R.id.spSize);
+    private void setupViews() {
+        spSize = (Spinner) findViewById(R.id.spSize);
         final ArrayAdapter<CharSequence> adapterSize = ArrayAdapter.createFromResource(this,
                 R.array.size_array, android.R.layout.simple_spinner_item);
         adapterSize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spSize.setAdapter(adapterSize);
+        spSize.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-        final Spinner spColor = (Spinner) findViewById(R.id.spColor);
+            @Override
+            public void onItemSelected(final AdapterView<?> parent, final View view, final int position, final long id) {
+                argSize = optionsSize[position];
+            }
+
+            @Override
+            public void onNothingSelected(final AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+
+        });
+
+        spColor = (Spinner) findViewById(R.id.spColor);
         final ArrayAdapter<CharSequence> adapterColor = ArrayAdapter.createFromResource(this,
                 R.array.color_array, android.R.layout.simple_spinner_item);
         adapterColor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spColor.setAdapter(adapterColor);
+        spColor.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-        final Spinner spType = (Spinner) findViewById(R.id.spType);
+            @Override
+            public void onItemSelected(final AdapterView<?> parent, final View view, final int position, final long id) {
+                argColor = optionsColor[position];
+            }
+
+            @Override
+            public void onNothingSelected(final AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+
+        });
+
+        spType = (Spinner) findViewById(R.id.spType);
         final ArrayAdapter<CharSequence> adapterType = ArrayAdapter.createFromResource(this,
                 R.array.type_array, android.R.layout.simple_spinner_item);
         adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spType.setAdapter(adapterType);
+        spType.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(final AdapterView<?> parent, final View view, final int position, final long id) {
+                argType = optionsType[position];
+            }
+
+            @Override
+            public void onNothingSelected(final AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+
+        });
+    }
+
+    public void onSettingSave(final View v) {
+        final String str = argSize + ' ' + argColor + ' ' + argType;
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
     }
 
     @Override
