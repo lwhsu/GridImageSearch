9bc5cecfff6b5ecf09b2a32b51d28a096ea35d93
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-11-24 01:50:47 +0800

    Show the result images

--- src/org/lwhsu/android/gridimagesearch/adapters/ImageResultsAdapter.java
+++ src/org/lwhsu/android/gridimagesearch/adapters/ImageResultsAdapter.java
@@ -2,20 +2,39 @@ package org.lwhsu.android.gridimagesearch.adapters;
 
 import java.util.List;
 
+import org.lwhsu.android.gridimagesearch.R;
 import org.lwhsu.android.gridimagesearch.models.ImageResult;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ImageResultsAdapter extends ArrayAdapter<ImageResult> {
     public ImageResultsAdapter(final Context context, final List<ImageResult> images) {
-        super(context, android.R.layout.simple_list_item_1, images);
+        super(context, R.layout.item_image_result, images);
     }
 
     @Override
-    public View getView(final int position, final View convertView, final ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+        final ImageResult imageInfo = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_image_result, parent, false);
+        }
+        final ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+        final TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        // Clear out image from last time
+        ivImage.setImageResource(0);
+        // Populate title and remote download image url
+        tvTitle.setText(imageInfo.title);
+        // Remotely download the image data in the background (with Picasso)
+        Picasso.with(getContext()).load(imageInfo.thumbUrl).into(ivImage);
+        // Return the completed view to be displayed
+        return convertView;
     }
 }
